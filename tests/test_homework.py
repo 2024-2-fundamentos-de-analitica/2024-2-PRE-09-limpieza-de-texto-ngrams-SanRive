cbfc336b7ece1e5a0bdaad6cde78ea49b27c0c5e
SRCrimson <sriverame@unal.edu.co>
2024-12-03 16:55:13 -0500

    Completed Assignment 9

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -20,26 +20,26 @@ def test_01():
 
     test = pd.read_csv("files/test.csv", index_col=None)
 
-    assert test.loc[0, "key"] == "alanapatcacsiciolilynnaonplppsatiyt"
-    assert test.loc[2, "key"] == "alanapatcacsiciolilynansonplppssatiyt"
-    assert test.loc[3, "key"] == "alancsdeelicllymonaodsmtiyt"
-    assert test.loc[7, "key"] == "alancadeeliclmlslymonaodstiyt"
-    assert test.loc[12, "key"] == "agalctcudugriclpltodprrariroststuuculur"
-    assert test.loc[17, "key"] == "aiesinirlinerls"
+    assert test.loc[0, "key"] == "acdeghinoqruy"
+    assert test.loc[1, "key"] == "acdegilmnoty"
+    assert test.loc[3, "key"] == "acdehioqrsu"
+    assert test.loc[6, "key"] == "acdehoqruy"
+    assert test.loc[12, "key"] == "acdeilmnoty"
+    assert test.loc[16, "key"] == "acdgilnoprstu"
 
     #
-    # Retorna error si la carpeta  no existe
+    # Retorna error si la carpeta output/ no existe
     if not os.path.exists("files/output.txt"):
         raise FileNotFoundError("File 'files/output.txt' not found")
 
     #
     # Lee el contenido del archivo output.txt
     dataframe = pd.read_csv("files/output.txt")
-    count = dataframe.groupby("cleaned_text").size()
+    count = dataframe.groupby("text").size()
 
     assert count.loc["AD-HOC QUERIES"] == 3
     assert count.loc["AGRICULTURAL PRODUCTION"] == 1
     assert count.loc["AIRLINE COMPANIES"] == 1
     assert count.loc["AIRLINES"] == 1
-    assert count.loc["ANALYTIC APPLICATIONS"] == 2
-    assert count.loc["ANALYTIC MODEL"] == 2
+    assert count.loc["ANALYTIC APPLICATIONS"] == 9
+    assert count.loc["ANALYTIC MODEL"] == 4
